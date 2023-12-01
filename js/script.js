9231f77bb7ff862c69029ddecb1d50bed421ed64
MariamZead <mariam.a.zead@gmail.com>
2023-12-01 10:29:23 +0200

    Disabling the sticky navbar and setting it as a fixed element.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,4 @@
 ///////////////////////////////////////////////////////////
-// Sticky navigation
-
-const sectionHeroEl = document.querySelector(".hero-section");
-
-const obs = new IntersectionObserver(
-  function (entries) {
-    const ent = entries[0];
-    console.log(ent);
-
-    if (!ent.isIntersecting) {
-      document.body.classList.add("sticky");
-    }
-
-    if (ent.isIntersecting) {
-      document.body.classList.remove("sticky");
-    }
-  },
-  {
-    // In the viewport
-    root: null,
-    threshold: 0,
-    rootMargin: "-80px",
-  }
-);
-obs.observe(sectionHeroEl);
-
-///////////////////////////////////////////////////////////
 // Make mobile navigation work
 
 const btnNavEl = document.querySelector(".btn-mobile-nav");
