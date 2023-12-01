fc5e417a15ae05fdc02fa2c0f8f084a20e160d43
MariamZead <mariam.a.zead@gmail.com>
2023-12-01 11:36:54 +0200

    Implementing behavior upon clicking mobile nav link

--- js/script.js
+++ js/script.js
@@ -7,3 +7,33 @@ const headerEl = document.querySelector(".header");
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
 });
+
+
+///////////////////////////////////////////////////////////
+// Smooth scrolling animation when clicking on mobile nav link
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("nav-link"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
